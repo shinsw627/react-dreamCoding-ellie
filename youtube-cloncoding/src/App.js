7ac7a93b8b8014d4eb87f6acca30ca961e2214f8
sungwoongshin <swshin@pntbiz.com>
2022-06-23 17:01:48 +0900

    fix: habitTracker 커밋오류 수정

--- youtube-cloncoding/src/App.js
+++ youtube-cloncoding/src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
   const [name, setName] = useState("ellie");
 
   useEffect(() => {
-    console.log("useEffect");
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA-jwEa-sFtDUe2SUYyd4HJMFhL2XrICfA",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
   }, []);
-  return <></>;
+  return <VideoList videos={videos} />;
 }
 
 export default App;
